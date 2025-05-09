4259e24c44e1e546c1b68a24cedc0d6e5f75baec
Bardia Moshiri <bardia@furilabs.com>
2025-05-08 21:28:26 -0400

    debian: initial packaging
    debian's version of aidl is stuck in android 10 which is not enough for generation of new aidl cpp and headers since it doesn't support stuff like vintf stability.
    on the other hand, we do need an up to date and functional aidl binary for our hybris-codegen project which hooks libbinder_ndk through hybris and allows us to compile HAL client code with very little to no modifications and without any gbinder hassle which we have been putting up with for years now (yuck)
    surprisingly debian has android-libbase and android-libcutils packaged up to api34 so all i had to do here is just pull aidl and write makefiles for it then updating packaging
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

---
+++ debian/aidl.mk
@@ -0,0 +1,14 @@
+NAME = aidl
+SOURCES = main.cpp
+CXXFLAGS += -std=c++17 -O0 -g -Wno-error -Wno-everything
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -L. -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -laidl-common -lbase -llog -lcutils -lgtest -latomic
+
+all: $(NAME)
+
+$(NAME): $(SOURCES) libaidl-common.so
+	$(CXX) $(SOURCES) -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
+
+clean:
+	rm -f $(NAME)
